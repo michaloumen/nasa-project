fbeba1f6eec4a08c9c2bde1087dbaa2913a85b74
michaloumen <michelle.mendonca@minu.co>
2023-02-10 09:47:52 -0300

    building a data access layer

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,11 +1,11 @@
-const { planets } = require('../../models/planets.model');
+const { getAllPlanets } = require('../../models/planets.model');
 
 //adding return to make sure the function stops executing
-function getAllPlanets(req, res) {
-  return res.status(200).json(planets)
+function httpGetAllPlanets(req, res) {
+  return res.status(200).json(getAllPlanets)
 };
 
 //return as an obj to export multiple functions
 module.exports = {
-  getAllPlanets
-}
+  httpGetAllPlanets
+};
--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { getAllPlanets } = require('./planets.controller');
+const { httpGetAllPlanets } = require('./planets.controller');
 //destructure the obj to use functions directly
 
 const planetsRouter = express.Router();
 
-planetsRouter.get('/planets', getAllPlanets);
+planetsRouter.get('/planets', httpGetAllPlanets());
 
 module.exports = planetsRouter;
