7c757eccea49219a80c2df4d6d950e5aa19cae4f
michaloumen <michelle.mendonca@minu.co>
2023-04-12 23:10:50 -0300

    populating the planets collection

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,11 +1,9 @@
 const { getAllPlanets } = require('../../models/planets.model');
 
-//adding return to make sure the function stops executing
-function httpGetAllPlanets(req, res) {
-  return res.status(200).json(getAllPlanets());
+async function httpGetAllPlanets(req, res) {
+  return res.status(200).json(await getAllPlanets());
 };
 
-//return as an obj to export multiple functions
 module.exports = {
   httpGetAllPlanets
 }
