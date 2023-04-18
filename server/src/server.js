dc2b6fece0031ecb463023bd184b7f3e067a5189
michaloumen <michelle.mendonca@minu.co>
2023-04-18 01:04:32 -0300

    refactor and updating testes for mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,32 +1,19 @@
 const http = require('http');
-const mongoose = require('mongoose');
 const app = require('./app');
+const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL = 'mongodb+srv://michellelmendonca:Micha123@cluster0.24ro1i5.mongodb.net/nasa?retryWrites=true&w=majority';
-
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-  console.log('MongoDB connection ready!');
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error('err');
-});
-
 async function startServer() {
-  // this way data is available for any request that ever comes into our server
-  await mongoose.connect(MONGO_URL);
+  await mongoConnect
   await loadPlanetsData();
 
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
   });
 };
+
 startServer();
-
-
-
